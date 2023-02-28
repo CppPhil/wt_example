c3d382c9f17f2082de6d13b74fa8e406e2ac4da7
Phillip Schackier <CppPhil@users.noreply.github.com>
2023-02-28 22:12:10 +0100

    Update

---
+++ run_debug.ps1
@@ -0,0 +1,1 @@
+& "$PSScriptRoot\build\Debug\wt_example_app.exe" --docroot . --http-address 0.0.0.0 --http-port 9090
---
+++ run_release.ps1
@@ -0,0 +1,1 @@
+& "$PSScriptRoot\build\Release\wt_example_app.exe" --docroot . --http-address 0.0.0.0 --http-port 9090
